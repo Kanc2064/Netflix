a71e339866464b55ee7aa8272bac3caf1c950770
Kanc2064 <139121950+Kanc2064@users.noreply.github.com>
2023-10-09 10:43:49 +0530

    Add Home folder

---
+++ Home.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-template-curly-in-string */
+import React, { useEffect, useState } from 'react'
+import "./home.scss"
+import Featured from '../../components/featured/Featured'
+import List from '../../components/list/List'
+import Navbar from '../../components/navbar/Navbar'
+import axios from 'axios'
+
+
+const Home = ({type}) => {
+  const [lists, setLists] = useState([]);
+  const [genre,setGenre] = useState(null);
+
+  useEffect(()=>{
+    const getRandomLists = async ()=>{
+      try {
+          const res = await axios.get(
+            `lists${type ? "?type=" + type : ""}${genre ? "&genre=" + genre : ""}`,{
+              headers:{
+                token :
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MGVhYjVlMGI2MWI2MTYwMDRmNTJmMiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY5NjA5NDQ3OSwiZXhwIjoxNjk2NTI2NDc5fQ.J3lzxAO0JNWmadI5ZSbmdoaEp_b_TRPn5tFC9e-v9Ng"  
+              },
+            }
+            );
+            setLists(res.data);
+      }catch(err){
+        console.log(err)
+      }
+    };
+    getRandomLists();
+  },[type,genre]);
+  return (
+    <div className="home">
+      <Navbar/>
+      <Featured type={type}/>
+      {lists.map((list)=>(
+      <List list={list}/>
+      ))}
+    </div>
+  )
+}
+
+export default Home
+
