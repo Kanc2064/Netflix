f5ed761620d1c30952b3af755a6c2e8c3ac3a13a
Kanc2064 <139121950+Kanc2064@users.noreply.github.com>
2023-10-09 10:45:15 +0530

    Add watch folder

---
+++ Watch.js
@@ -0,0 +1,20 @@
+import { ArrowBackOutlined } from '@material-ui/icons'
+import React from 'react'
+import { useLocation } from 'react-router-dom'
+import "./watch.scss"
+
+export default function Watch() {
+  //const location = useState([]);
+  const location = useLocation();
+  console.log(location)
+  //const movie = location.movie;
+  return (
+    <div className="watch">
+      <div className="back">
+          <ArrowBackOutlined/>
+          Home
+      </div>
+      <video className="video" autoPlay progress controls src="https://www.vecteezy.com/free-videos/nature"/>
+      </div>
+  )
+}
